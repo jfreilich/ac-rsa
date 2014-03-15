d50f04dff262ff0c66bf996640efe00a89f8e21a
Vinh Tran <vinh_tran@brown.edu>
2014-03-15 15:07:30 -0400

    Fixed Path Problem by passing it in argv

--- src/main.py
+++ src/main.py
@@ -1,13 +1,10 @@
 import sys
-
-
 import audiolab
-
 import numpy as np
 
 
 def main():
-    x, fs, nbits = audiolab.wavread('file1.wav')
+    x, fs, nbits = audiolab.wavread(argv[0])
     audiolab.play(x, fs)
     N = 38*fs    # four seconds of audio
     X = scipy.fft(x[:N])
@@ -22,45 +19,45 @@ def main():
 def main2():
 
 
-	#Python 2.x:
-	#from __future__ import division
+   #Python 2.x:
+   #from __future__ import division
 
-	Fs = 48000
-	f = np.arange(1, 9) * 2000
-	t = np.arange(8 * Fs) / Fs 
-	x = np.empty(t.shape)
-	for i in range(8):
-	    x[i*Fs:(i+1)*Fs] = np.cos(2*np.pi * f[i] * t[i*Fs:(i+1)*Fs])
+   Fs = 48000
+   f = np.arange(1, 9) * 2000
+   t = np.arange(8 * Fs) / Fs 
+   x = np.empty(t.shape)
+   for i in range(8):
+       x[i*Fs:(i+1)*Fs] = np.cos(2*np.pi * f[i] * t[i*Fs:(i+1)*Fs])
 
-	w = np.hamming(512)
-	Pxx, freqs, bins = mlab.specgram(x, NFFT=512, Fs=Fs, window=w, 
-			                 noverlap=464)
+   w = np.hamming(512)
+   Pxx, freqs, bins = mlab.specgram(x, NFFT=512, Fs=Fs, window=w, 
+                          noverlap=464)
 
-	#plot the spectrogram in dB
+   #plot the spectrogram in dB
 
-	Pxx_dB = np.log10(Pxx)
-	pyplot.subplots_adjust(hspace=0.4)
+   Pxx_dB = np.log10(Pxx)
+   pyplot.subplots_adjust(hspace=0.4)
 
-	pyplot.subplot(211)
-	ex1 = bins[0], bins[-1], freqs[0], freqs[-1]
-	pyplot.imshow(np.flipud(Pxx_dB), extent=ex1)
-	pyplot.axis('auto')
-	pyplot.axis(ex1)
-	pyplot.xlabel('time (s)')
-	pyplot.ylabel('freq (Hz)')
+   pyplot.subplot(211)
+   ex1 = bins[0], bins[-1], freqs[0], freqs[-1]
+   pyplot.imshow(np.flipud(Pxx_dB), extent=ex1)
+   pyplot.axis('auto')
+   pyplot.axis(ex1)
+   pyplot.xlabel('time (s)')
+   pyplot.ylabel('freq (Hz)')
 
-	#zoom in at t=4s to show transient
+   #zoom in at t=4s to show transient
 
-	pyplot.subplot(212)
-	n1, n2 = int(3.991/8*len(bins)), int(4.009/8*len(bins))
-	ex2 = bins[n1], bins[n2], freqs[0], freqs[-1]
-	pyplot.imshow(np.flipud(Pxx_dB[:,n1:n2]), extent=ex2)
-	pyplot.axis('auto')
-	pyplot.axis(ex2)
-	pyplot.xlabel('time (s)')
-	pyplot.ylabel('freq (Hz)')
+   pyplot.subplot(212)
+   n1, n2 = int(3.991/8*len(bins)), int(4.009/8*len(bins))
+   ex2 = bins[n1], bins[n2], freqs[0], freqs[-1]
+   pyplot.imshow(np.flipud(Pxx_dB[:,n1:n2]), extent=ex2)
+   pyplot.axis('auto')
+   pyplot.axis(ex2)
+   pyplot.xlabel('time (s)')
+   pyplot.ylabel('freq (Hz)')
 
-	pyplot.show()
+   pyplot.show()
 
 if __name__ == '__main__':
     main()
